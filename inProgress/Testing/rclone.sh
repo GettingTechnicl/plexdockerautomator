8a012cc2df1b91042f4dcfad0ce084ae325ed193
Terry Gluff <terry@pclanservices.com>
2020-05-20 11:21:56 -0500

    upd

--- inProgress/Testing/rclone.sh
+++ inProgress/Testing/rclone.sh
@@ -2,3 +2,12 @@
 
 ## This is the official Rclone docker containers
 # https://hub.docker.com/r/rclone/rclone
+
+
+docker run -d --name rclone \
+--restart=unless-stopped \
+--cap-add SYS_ADMIN \
+--device /dev/fuse \
+--security-opt apparmor:unconfined \
+-v /docker_exchange_host:/docker_exchange:shared \
+rclone/rclone mount gdrive:Cloud /docker_exchange/gdrive --config /docker_exchange/config/rclone/rclone.conf --allow-other --allow-non-empty --fast-list --buffer-size 256M --drive-chunk-size 32M --dir-cache-time 96h --log-level DEBUG --timeout 1h --tpslimit 4 --umask 002 --rc --vfs-cache-mode writes --vfs-read-chunk-size 128M --vfs-read-chunk-size-limit off --cache-dir /docker_exchange/cache
---
+++ inProgress/Testing/sonarr.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## New Sonarr version
+
+
+##need variable to connect to nzbget, right now only connects ip, but what about other systems?
+sudo docker run -d --name sonarr-sma \
+    --restart=unless-stopped \
+    --cap-add SYS_ADMIN \
+    -e PUID=1000 \
+    -e PGID=1000 \
+    -e TZ=America/Chicago \
+    -p 8989:8989 \
+    -v /docker_exchange_host/config/sonarr:/config \
+    -v /docker_exchange_host/config/sma:/usr/local/sma/config \
+    -v /docker_exchange_host/gdrive/Tv_Shows:/tv \
+    -v /docker_exchange_host/Downloads/sonarr:/downloads \
+    mdhiggins/sonarr-sma:preview
