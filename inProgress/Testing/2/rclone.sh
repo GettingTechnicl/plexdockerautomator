f3040f63e2744f38a6b0dc9c725b9d9ecdbab8b7
Terry Gluff <terry@pclanservices.com>
2020-05-21 13:43:55 -0500

    upd

--- inProgress/Testing/2/rclone.sh
+++ inProgress/Testing/2/rclone.sh
@@ -12,4 +12,20 @@ docker run -d --name rclone \
 --security-opt apparmor:unconfined \
 -v /opt/tmp/config:/config \
 -v /DATA/media:/data:shared \
-rclone/rclone mount gdrive:Cloud /data/gdrive --config /config/rclone/rclone.conf --allow-other --allow-non-empty --min-age 2m --dir-cache-time=160h --cache-chunk-size=10M --cache-chunk-total-size=10G --cache-info-age=168h --cache-workers=6 --attr-timeout=1s --modify-window 1s --drive-use-trash=false --cache-writes --buffer-size=0M --daemon-timeout=10m --tpslimit 5"
+rclone/rclone mount gdrive:Cloud /DATA/gdrive \
+--config /config/rclone/rclone.conf \
+--allow-other \
+--allow-non-empty \
+--fast-list \
+--buffer-size 256M \
+--drive-chunk-size 32M \
+--dir-cache-time 96h \
+--log-level DEBUG \
+--timeout 1h \
+--tpslimit 4 \
+--umask 002 \
+--rc \
+--vfs-cache-mode writes \
+--vfs-read-chunk-size 128M \
+--vfs-read-chunk-size-limit off \
+--cache-dir /DATA/cache \
