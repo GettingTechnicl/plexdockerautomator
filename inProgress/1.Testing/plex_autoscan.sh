1c38b956e022177c084b93ae0dd36dbac8f13af8
Terry G <gettingtechnicl@gmail.com>
2020-05-22 01:44:33 -0500

    plex_autoscan.sh

---
+++ inProgress/1.Testing/plex_autoscan.sh
@@ -0,0 +1,4 @@
+https://github.com/l3uddz/plex_autoscan#docker
+
+## Doesn't appear to support docker, appears that it wants to be ran on the host,
+## although supports plex being in a docker, and dockerized applications
