8f9ad1749856f0186ff92ac5f32f39301a07ce38
Terry Gluff <terry@pclanservices.com>
2020-05-21 20:56:30 -0500

    upd plex ports

--- inProgress/Testing/plex.sh
+++ inProgress/Testing/plex.sh
@@ -2,6 +2,15 @@ docker run -d --name plex \
 --network=host \
 -e TZ="America/Chicago" \
 -e PLEX_CLAIM="<claimToken>" \
+-p 32400:32400/tcp \
+-p 3005:3005/tcp \
+-p 8324:8324/tcp \
+-p 32469:32469/tcp \
+-p 1900:1900/udp \
+-p 32410:32410/udp \
+-p 32412:32412/udp \
+-p 32413:32413/udp \
+-p 32414:32414/udp \
 -v /opt/tmp/config/plexdb:/config \
 -v /mnt/ramdisk:/transcode \
 -v /DATA/media:/data \
