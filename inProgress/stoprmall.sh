ca3d772c6502278ac2fe4d471c301292dc779693
hoyane <chrisflynninbox@gmail.com>
2020-06-24 21:29:34 -0500

    tweaks

---
+++ inProgress/stoprmall.sh
@@ -0,0 +1,23 @@
+service jackett stop
+service lidarr stop
+service mylar stop
+service nzbget stop
+service ombi stop
+service plex stop
+service radarr stop
+service rclone-cache stop
+service rclone-vfs stop
+service sonarr stop
+
+docker rm jackett
+docker rm lidarr
+docker rm mylar
+docker rm nzbget
+docker rm ombi
+docker rm plex
+docker rm radarr
+docker rm rclone-cache
+docker rm rclone-vfs
+docker rm sonarr
+
+docker system prune -a
