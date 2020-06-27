ce3e9ddb5773cafe5491c7960a4af16c291f1577
hoyane <chrisflynninbox@gmail.com>
2020-06-26 22:39:39 -0500

    x

--- inProgress/3.db-config_complete/mergerfs.sh
+++ inProgress/3.db-config_complete/mergerfs.sh
@@ -3,14 +3,15 @@
 #https://hub.docker.com/r/hotio/mergerfs
 
 # mergerfs Config
-docker run --name mergerfs \
+docker run -d --name mergerfs \
   --security-opt apparmor:unconfined \
   --cap-add SYS_ADMIN \
   --device /dev/fuse \
   --restart unless-stopped \
-  -e PUID=1002 \
+  -e PUID=0 \
   -e PGID=0 \
   -e TZ=America/Chicago \
+  -v /DATA/tmp/test:/local \
   -v /DATA/tmp/rclone-cache:/gdrive \
   -v /DATA/tmp/mergedhost:/merged:shared \
   hotio/mergerfs -o defaults,direct_io,sync_read,allow_other,category.action=all,category.create=ff \
