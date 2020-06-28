032b2089e52a2154f3e26476d0f38fe36d297f9c
hoyane <chrisflynninbox@gmail.com>
2020-06-28 14:17:47 -0500

    x

--- inProgress/1.Testing/deployAll.sh
+++ inProgress/1.Testing/deployAll.sh
@@ -151,7 +151,7 @@ docker ${dCMD} --name rclone-sync \
 -e FORCE_SYNC=1 \
 -e CHECK_URL=https://hchk.io/hchk_uuid \
 -e RCLONE_OPTS="--delete-after --log-file /config/rclone/rclone-sync.log --log-level INFO" \
--e SYNC_OPTS=-v
+-e SYNC_OPTS=-v \
 bcardiff/rclone
 
 
