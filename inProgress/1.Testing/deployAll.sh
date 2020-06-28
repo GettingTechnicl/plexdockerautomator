3338913709a80935aa9107649bb6dee8f32846d8
hoyane <chrisflynninbox@gmail.com>
2020-06-28 12:45:45 -0500

    x

--- inProgress/1.Testing/deployAll.sh
+++ inProgress/1.Testing/deployAll.sh
@@ -159,10 +159,12 @@ docker ${dCMD} --name mergerfs \
   --cap-add SYS_ADMIN \
   --device /dev/fuse \
   --restart unless-stopped \
+  -e PUID=0 \
+  -e PGID=0 \
   -e TZ=${tZone} \
-  -v ${rioDir}/tmp_upload:/local \
-  -v ${rioDir}/rclone-cache:/cloud_drive \
-  -v ${rioDir}/mergerfs:/merged:shared \
+  -v /DATA/tmp/tmp_upload:/local \
+  -v /DATA/tmp/rclone-cache:/cloud_drive \
+  -v /DATA/tmp/mergerfs:/merged:shared \
   hotio/mergerfs -o defaults,direct_io,sync_read,allow_other,category.action=all,category.create=ff \
   /local:/cloud_drive \
   /merged
