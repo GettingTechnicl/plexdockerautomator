c9a3d46f598b0caca2fd9906ca9a551021984309
Terry Gluff <terry@pclanservices.com>
2020-06-28 21:28:02 -0500

    rclonemovedocker-BETA

---
+++ inProgress/1.Testing/rclone-move-docker.sh
@@ -0,0 +1,11 @@
+docker run --rm -it \
+-v $(pwd)/config:/config \
+-v /DATA/tmp/tmp_upload:/source \
+-e RCLONE_CMD="move"
+-e SYNC_SRC="/source" \
+-e SYNC_DEST="gdrive:Cloud" \
+-e TZ="America/Chicago" \
+-e CRON="0 0 * * *" \
+-e CRON_ABORT="0 6 * * *" \
+-e FORCE_SYNC=1
+-e CHECK_URL=https://hchk.io/hchk_uuid pfidr/rclone
