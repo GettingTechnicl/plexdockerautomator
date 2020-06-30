c677b4a6875d37e59914b2dbbad84a28b8df06a1
hoyane <chrisflynninbox@gmail.com>
2020-06-29 23:02:49 -0500

    x

--- inProgress/3.db-config_complete/radarr.sh
+++ inProgress/3.db-config_complete/radarr.sh
@@ -3,15 +3,14 @@
 #https://hub.docker.com/r/linuxserver/radarr
 
 # Radarr Config
-docker run -d --name radarr-v3 \
-  --restart unless-stopped \
+docker run --name radarr \
   --network=host \
+  -e ADVERTISE_IP="localhost:7878/" \
   -e PUID=0 \
   -e PGID=0 \
-  -e ADVERTISE_IP="localhost:7878/" \
-  -e TZ=America/Chicago \
   -v /opt/tmp/config/radarr:/config \
+  -v /opt/tmp/config/sma:/usr/local/sma/config \
   -v /DATA/tmp/rclone-cache/Movies:/movies \
   -v /DATA/tmp/rclone-cache/Stand_Ups:/standups \
-  -v /DATA/tmp/Downloads:/downloads \
+  -v /DATA/tmp/Downloads/radarr/nzbget:/downloads/radarr/nzbget \
   mdhiggins/radarr-sma:preview
