8c82d074599eac7ea2785df41bd1c15fbf85117f
Terry Gluff <terry@pclanservices.com>
2020-07-02 22:35:34 -0500

    Fix

--- inProgress/1.Testing/deployAll.sh
+++ inProgress/1.Testing/deployAll.sh
@@ -311,7 +311,7 @@ docker ${dCMD} --name sonarr \
 
 
 # Copies SystemD service scripts to systemD
-sudo cp ${target_PWD}/Systemd/* ${target_PWD}/workspace.local/
+sudo cp -a ${target_PWD}/Systemd/. ${target_PWD}/workspace.local
 sudo sed -i "s|CACHEPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-cache.service
 sudo sed -i "s|VFSPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-vfs.service
 sudo sed -i "s|MERGERPATH|${rioDir}|g" ${target_PWD}/workspace.local/mergerfs.service
--- inProgress/1.Testing/rewrites.sh
+++ inProgress/1.Testing/rewrites.sh
@@ -5,8 +5,8 @@ target_PWD="$(readlink -f .)"
 
 
 # Copies SystemD service scripts to systemD
-sudo cp -a ${target_PWD}/Systemd/. ${target_PWD}/workspace
-sudo sed -i "s|CACHEPATH|${rioDir}|g" ${target_PWD}/workspace/rclone-cache.service
-sudo sed -i "s|VFSPATH|${rioDir}|g" ${target_PWD}/workspace/rclone-vfs.service
-sudo sed -i "s|MERGERPATH|${rioDir}|g" ${target_PWD}/workspace/mergerfs.service
-#sudo mv ${target_PWD}/workspace/* /etc/systemd/system/
+sudo cp -a ${target_PWD}/Systemd/. ${target_PWD}/workspace.local
+sudo sed -i "s|CACHEPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-cache.service
+sudo sed -i "s|VFSPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-vfs.service
+sudo sed -i "s|MERGERPATH|${rioDir}|g" ${target_PWD}/workspace.local/mergerfs.service
+#sudo mv ${target_PWD}/workspace.local/* /etc/systemd/system/
