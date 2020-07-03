647cf9535c0f0e7b70ff55b0436b3386fcb787ec
Terry Gluff <terry@pclanservices.com>
2020-07-02 21:59:06 -0500

    systemD rewrites

--- inProgress/1.Testing/deployAll.sh
+++ inProgress/1.Testing/deployAll.sh
@@ -311,7 +311,12 @@ docker ${dCMD} --name sonarr \
 
 
 # Copies SystemD service scripts to systemD
-sudo mv ${target_PWD}/Systemd/* /etc/systemd/system/
+sudo cp ${target_PWD}/Systemd/* ${target_PWD}/workspace.local/
+sudo sed -i "s|CACHEPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-cache.service
+sudo sed -i "s|VFSPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-vfs.service
+sudo sed -i "s|MERGERPATH|${rioDir}|g" ${target_PWD}/workspace.local/mergerfs.service
+sudo mv ${target_PWD}/workspace.local/* /etc/systemd/system/
+
 sudo systemctl enable rclone-cache.service
 sudo systemctl enable rclone-vfs.service
 sudo systemctl enable rclone-move.service
