02e6073c4f8c7ad417e50bc7711bd5af574aee2e
Terry Gluff <terry@pclanservices.com>
2020-07-02 22:28:32 -0500

    ss

--- inProgress/1.Testing/rewrites.sh
+++ inProgress/1.Testing/rewrites.sh
@@ -2,7 +2,7 @@
 
 # Copies SystemD service scripts to systemD
 sudo cp ${target_PWD}/Systemd/* ${target_PWD}/workspace
-#sudo sed -i "s|CACHEPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-cache.service
-#sudo sed -i "s|VFSPATH|${rioDir}|g" ${target_PWD}/workspace.local/rclone-vfs.service
-#sudo sed -i "s|MERGERPATH|${rioDir}|g" ${target_PWD}/workspace.local/mergerfs.service
-#sudo mv ${target_PWD}/workspace.local/* /etc/systemd/system/
+#sudo sed -i "s|CACHEPATH|${rioDir}|g" ${target_PWD}/workspace/rclone-cache.service
+#sudo sed -i "s|VFSPATH|${rioDir}|g" ${target_PWD}/workspace/rclone-vfs.service
+#sudo sed -i "s|MERGERPATH|${rioDir}|g" ${target_PWD}/workspace/mergerfs.service
+#sudo mv ${target_PWD}/workspace/* /etc/systemd/system/
