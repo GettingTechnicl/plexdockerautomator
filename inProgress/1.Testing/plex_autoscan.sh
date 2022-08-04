a567708b403943a281a1632156bc52177f7fc273
TG <gettingtechnicl@gmail.com>
2022-08-04 08:58:07 -0500

    cleanup of legacy docker deployments

--- inProgress/1.Testing/plex_autoscan.sh
+++
@@ -1,4 +0,0 @@
-https://github.com/l3uddz/plex_autoscan#docker
-
-## Doesn't appear to support docker, appears that it wants to be ran on the host,
-## although supports plex being in a docker, and dockerized applications
--- inProgress/3.db-config_complete/deluge.sh
+++
@@ -1,9 +0,0 @@
-docker run --net=container:vpn --name=deluge \
-  -e ADVERTISE_IP="localhost:8112/" \
-  -e PUID=0 \
-  -e PGID=0 \
-  -e TZ=America/Chicago \
-  -e DELUGE_LOGLEVEL=error \
-  -v /opt/tmp/config/deluge:/config \
-  -v /DATA/tmp/Downloads/torrent:/downloads \
-  linuxserver/deluge
